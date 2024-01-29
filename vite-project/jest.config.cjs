638479180c34097355f44abf1c3fe44e0388b33d
Grashal Vishwa <89205098+vgrashal16@users.noreply.github.com>
2024-01-29 15:35:19 +0530

    jest setup

---
+++ vite-project/jest.config.cjs
@@ -0,0 +1,12 @@
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'jsdom',
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+  coverageReporters: ['lcov', 'text', 'html'],
+  setupFilesAfterEnv: ['./src/setupTests.ts'],
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/src/$1',
+    "\\.(jpg|jpeg|png|gif|webp|svg|ico|ttf|woff|woff2|eot)$": "identity-obj-proxy",
+  },
+  setupFilesAfterEnv: ['<rootDir>/src/setupTests.ts'],
+};
